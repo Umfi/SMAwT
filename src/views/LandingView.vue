614f15c947cc8d1e4dbdf3366ca76db339ac875c
Michael Umfahrer <michael@umfahrer.dev>
2023-02-22 18:20:14 +0100

    Transaltion

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -6,16 +6,10 @@
     <div class="row justify-content-center mt-3 mb-5">
       <div class="col-8">
             <p class="lead">
-              The Internet makes it so easy to communicate with family, friends—everybody. We send
-              messages, share photos, join chats and livestream—sometimes without thinking about
-              who can see them, either right then or at a completely different time. A picture or post
-              you think is funny and harmless could be misunderstood by people you never thought
-              would see it—now or way off in the future. Feelings could get hurt. Someone who
-              doesn’t get the joke could think you’re mean just because they don’t know you. Once
-              somethingʼs out there, it’s hard to take it back, and people can copy, screenshot it and
-              share it. 
+              {{ $t('The Internet makes it so easy to communicate with family, friends—everybody. We send messages, share photos, join chats and livestream—sometimes without thinking about who can see them, either right then or at a completely different time. A picture or post you think is funny and harmless could be misunderstood by people you never thought would see it—now or way off in the future. Feelings could get hurt. Someone who doesn’t get the joke could think you’re mean just because they don’t know you. Once somethingʼs out there, it’s hard to take it back, and people can copy, screenshot it and share it.') }} 
               <br><br>
-              <b>This tool is designed to help you learn how to use social media safely.</b>
+              <b>
+                {{ $t('This tool is designed to help you learn how to use social media safely.') }}</b>
             </p>
       </div>
     </div>
