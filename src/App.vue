2653c2a676cb71b6e67e8c272be1c1df45fb4699
Michael Umfahrer <michael@umfahrer.dev>
2023-03-01 18:42:05 +0100

    Add load/save function

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,14 @@
             </div>
 
             <div class="float-end">
-              <select class="form-select" v-model="language">
-                <option value="en">🇬🇧&emsp;EN</option>
-                <option value="de">🇩🇪&emsp;DE</option>
-              </select>
+              <input type="file" id="uploadmyfile" @change="requestUploadFile" style="display: none;"/>
+              <button type="button" class="btn btn-secondary me-2" @click="loadProgress" title="Spielstand laden">
+                <i class="fa-solid fa-folder-open"></i>
+              </button>
+
+              <button type="button" class="btn btn-secondary" @click="saveProgress" title="Spielstand speichern">
+                <i class="fa-solid fa-floppy-disk"></i>
+              </button>
             </div>
         </div>
     </nav>
@@ -41,6 +45,59 @@ export default {
   watch:{
     language: function (val) {
       this.$i18n.locale = val;
+    }
+  },
+  methods: {
+    loadProgress() {
+      this.$el.querySelector('#uploadmyfile').click();
+    },
+    requestUploadFile() {
+      var src = this.$el.querySelector('#uploadmyfile')
+
+      var fileReader=new FileReader();
+
+      fileReader.onload=function(){
+        try {
+          const data = JSON.parse(fileReader.result);
+          localStorage.setItem('sometra_points', data.points);
+          localStorage.setItem('sometra_user', data.user);
+          localStorage.setItem('sometra_levels', data.levels);
+          localStorage.setItem('sometra_preTest', data.preTestDone);
+
+          window.location.reload();
+        } catch (e) {
+          alert('Fehler beim Laden der Datei');
+          return;
+        }
+          
+      }
+
+      fileReader.readAsText(src.files[0]);
+    },
+    saveProgress() {
+              const data =  {
+            points: localStorage.getItem('sometra_points'),
+            user: localStorage.getItem('sometra_user'),
+            levels: localStorage.getItem('sometra_levels'),
+            preTestDone: localStorage.getItem('sometra_preTest'),
+        }
+
+         const textToBLOB = new Blob([JSON.stringify(data)], { type: 'text/plain' });
+         const sFileName = 'smawt.json';	
+
+        let newLink = document.createElement("a");
+        newLink.download = sFileName;
+
+        if (window.webkitURL != null) {
+            newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+        }
+        else {
+            newLink.href = window.URL.createObjectURL(textToBLOB);
+            newLink.style.display = "none";
+            document.body.appendChild(newLink);
+        }
+
+        newLink.click(); 
     }
   }
 }
